592f11cd9aa0a6aeae953bb91c1d629d476d056a
aliwerdev <aliwerdev@gmail.com>
2022-12-13 11:11:40 +0500

    first commit

--- routers/codingspaceRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { getCodes, getCodeTypes } = require("../pages/dashboard/codingspace");
-
-const router = Router();
-
-router.get("/", getCodes);
-router.get("/types", getCodeTypes);
-
-module.exports = router;
